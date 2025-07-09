6759a072bb19a4d2d8414937f18400b6dadb6ae3
hereimsarif <sarif@sidatasolutions.com>
2025-07-10 02:17:47 +0530

    added tatapower

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -19,6 +19,7 @@ const popularStocks = [
   { symbol: "INFY", name: "Infosys" },
   { symbol: "ICICIBANK", name: "ICICI Bank" },
     { symbol: "AXISBANK", name: "AXIS Bank" },
+     { symbol: "TATAPOWER", name: "TATAPOWER" },
 ];
 
 export default function HomeScreen() {
